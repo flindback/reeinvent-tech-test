7f5adbb4f435a2bcf592c5738d18ba4e31bc6762
flindback <felicia.lindback@outlook.com>
2023-11-04 10:34:12 +0100

    Add initial test cases for synonym functionality

---
+++ server/synonyms/index.js
@@ -0,0 +1,7 @@
+const checkIfSynonym = (word, synonym) => {
+  return null;
+}
+
+const addSynonym = (word, synonym) => {
+    return null;
+}
---
+++ server/synonyms/synonyms.test.js
@@ -0,0 +1,20 @@
+import { expect, test } from "bun:test";
+
+
+/**
+ * Add synonyms
+ */
+
+/**
+ * Check if two words are synonyms
+ */
+
+
+/**
+ * Add a new word without making it a synonym
+ */
+
+
+/**
+ * Test that if A is a synonym to B, and B is a synonym to C, C has to be a synonym to A.
+ */
