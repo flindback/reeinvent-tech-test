694e35b37dedcc2d46848dc7e5115c654b821e55
flindback <felicia.lindback@outlook.com>
2023-11-08 14:56:00 +0100

    Try adding API key to Cloud Run instance - simplest possible implementation

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 import { SynonymsService } from "./synonyms";
 
+const API_KEY = process.env.API_KEY;
+
 const synonyms = new SynonymsService();
 
 const addSynonymsHandler = ({ words = [] }) => {
@@ -62,6 +64,7 @@ const setCorsHeaders = (res, origin) => {
 Bun.serve({
   port: 8080,
   async fetch(req) {
+    console.log(API_KEY);
     if (req.method === "OPTIONS") {
       const res = new Response("Departed", CORS_HEADERS);
       return res;
